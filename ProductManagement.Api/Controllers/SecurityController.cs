0dbaeec24e7a01bda501e3d53d7c7709e437a7c0
colakkonur <colakkonur@gmail.com>
2022-09-29 10:59:15 +0300

    Bux

--- ProductManagement.Api/Controllers/SecurityController.cs
+++ ProductManagement.Api/Controllers/SecurityController.cs
@@ -31,6 +31,10 @@ public class SecurityController : ControllerBase
         {
             return Unauthorized(response);
         }
+        
+        
+        
+        
         return Ok(response);
     }
 }
